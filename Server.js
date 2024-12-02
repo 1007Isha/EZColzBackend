16155074ef9df6d976b4192de955fae222ed6b9a
1007Isha <97464552+1007Isha@users.noreply.github.com>
2024-12-02 15:43:35 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,24 +1,3 @@
-// const express  = require("express")
-// const app = express()
-// let PORT = 8080
-
-// const sendMail = require("./controllers/sendMail")
-// app.get("/", (req,res) => {
-//     res.send("I am a server")
-// })
-
-// app.get("/mail", sendMail)
-// const start  = async() =>{
-//     try{
-//         app.listen(PORT,()=> {
-//             console.log(`I am active in port number ${PORT}`)
-//         })
-//     }
-//     catch(error){}
-// }
-// start()
-
-
 const express = require('express');
 const mongoose = require("mongoose");
 const http = require('http');
