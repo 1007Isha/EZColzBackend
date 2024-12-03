fbe8e0e115629015cb6bd0ae472cfcff20aa97e0
1007Isha <97464552+1007Isha@users.noreply.github.com>
2024-12-03 14:16:50 +0530

    Update password.js trial3

--- Utils/password.js
+++ Utils/password.js
@@ -1,3 +1,28 @@
+// const bcrypt = require('bcryptjs');
+
+// // Generate a random 6-digit password containing only numbers
+// const generatePassword = () => {
+//   return Math.floor(100000 + Math.random() * 900000); // Generates a random 6-digit number
+// };
+
+// // Hash a password
+// const hashPassword = async (password) => {
+//   const salt = await bcrypt.genSalt(10);
+//   return bcrypt.hash(password, salt);
+// };
+
+// // Compare a password
+// const comparePassword = async (password, hashedPassword) => {
+//   return bcrypt.compare(password, hashedPassword);
+// };
+
+// module.exports = {
+//   generatePassword,
+//   hashPassword,
+//   comparePassword
+// };
+
+
 const bcrypt = require('bcryptjs');
 
 // Generate a random 6-digit password containing only numbers
